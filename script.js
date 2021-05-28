66c82995189c37eb3ac969f7997cd997ae99dbce
Abdullahi Muritala <oabdullahi37@gmail.com>
2021-05-28 19:33:42 +0100

    Made player move a method for all player object created with the factory function, created the gameFlow function

--- script.js
+++ script.js
@@ -1,36 +1,44 @@
-const Module = (function(){
-    // Array of gameboard starting content
-    const gameboard = ["X","X","O","O","O","O","X","O","O"];
-    // Factory to create as many player as needed
+// Remains 5, 6 and 7
+const myModule = (function () {
+    // Player Factory function
     const player = function (name, selector) {
-        name = name;
-        selector = selector;
-        return {
-            name, selector
-        }
+    name = name;
+    selector = selector;
+    playerMove = function (e) {
+        gameboard[e.target.dataset.index] = selector;
     }
+    return {
+        name, selector, playerMove
+    }
+}
+    const gameboard = ["","","","","","","","",""]
+    // Player1 and Player2 with selectors
+    const player1 = player("Opey", "X");
+    const player2 = player("Yemi", "O");
+    let turn = true;
 
     // Grab DOM Elements we need to manipulate
     const doc = document;
     const buttons = doc.querySelectorAll("#board button");
-    
-    // Add Event Listener to the buttons
-    buttons.forEach(button => button.addEventListener("click", selectCell));
-    // Function to select each cell
-    function selectCell(e) {
-        let index = e.target.dataset.index;
-        gameboard[index] = "X";
+
+    buttons.forEach(button => button.addEventListener("click", gameFlow));
+
+    function gameFlow(e) {
+        if (turn) {
+            if (gameboard[e.target.dataset.index] !== "") return;
+            player1.playerMove(e);
+            turn = false;
+        } else {
+            if (gameboard[e.target.dataset.index] !== "") return;
+            player2.playerMove(e);
+            turn = true;
+        }
         displayController();
     }
-    // Display Controller
     const displayController = function () {
         buttons.forEach(button => {
-            let index = button.dataset.index;
-            button.textContent = gameboard[index];
+            button.textContent = gameboard[button.dataset.index];
         })
     }
     displayController();
-    // Player1 and Player2 with selectors
-    const player1 = player("Opey", "X");
-    const player2 = player("Yemi", "O");
 })();
