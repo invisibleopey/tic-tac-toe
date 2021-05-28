947edcbd5a3875fdc1e51dfc2a037c36fcc2c479
Abdullahi Muritala <oabdullahi37@gmail.com>
2021-05-28 21:45:29 +0100

    Worked out the winning logic and winner message

--- script.js
+++ script.js
@@ -16,29 +16,91 @@ const myModule = (function () {
     const player1 = player("Opey", "X");
     const player2 = player("Yemi", "O");
     let turn = true;
+    let round = 1;
+    let gameOver = false;
 
     // Grab DOM Elements we need to manipulate
     const doc = document;
     const buttons = doc.querySelectorAll("#board button");
+    const resultBoard = doc.querySelector("#result-board");
 
     buttons.forEach(button => button.addEventListener("click", gameFlow));
 
     function gameFlow(e) {
+        if (round === 9) {
+            resultBoard.textContent = "Draw! Restart Game"
+        }
+        if (gameOver) return
+        if (gameboard[e.target.dataset.index] !== "") return;
         if (turn) {
-            if (gameboard[e.target.dataset.index] !== "") return;
             player1.playerMove(e);
             turn = false;
         } else {
-            if (gameboard[e.target.dataset.index] !== "") return;
             player2.playerMove(e);
             turn = true;
         }
+        round++
         displayController();
+        getWinner();
     }
     const displayController = function () {
         buttons.forEach(button => {
             button.textContent = gameboard[button.dataset.index];
         })
+    }
+    function gotWinner(winnerBtn) {
+        if (winnerBtn.textContent === "X") {
+            resultBoard.textContent = `${player1.name} won!`
+        }
+        if (winnerBtn.textContent === "O") {
+            resultBoard.textContent = `${player2.name} won!`
+        }
+        gameOver = true;
+    }
+    function getWinner() {
+        // let btn1 = buttons[0], btn2 = buttons[1], btn3 = buttons[2], btn4 = buttons[3];
+        // let btn5 = buttons[4], btn6 = buttons[5], btn7 = buttons[6], btn8 = buttons[7];
+        // let btn9 = buttons[8];
+        if ((buttons[0].textContent !== "") && (buttons[0].textContent === 
+                buttons[1].textContent) && buttons[1].textContent === 
+                    buttons[2].textContent) {
+                        gotWinner(buttons[0]);
+                    };
+        if ((buttons[3].textContent !== "") && (buttons[3].textContent === 
+                buttons[4].textContent) && buttons[4].textContent === 
+                    buttons[5].textContent) {
+                        gotWinner(buttons[3]);
+                    };
+        if ((buttons[6].textContent !== "") && (buttons[6].textContent === 
+                buttons[7].textContent) && buttons[7].textContent === 
+                    buttons[8].textContent) {
+                        gotWinner(buttons[6]);
+                    };
+        if ((buttons[0].textContent !== "") && (buttons[0].textContent === 
+                buttons[3].textContent) && buttons[3].textContent === 
+                    buttons[8].textContent) {
+                        gotWinner(buttons[0]);
+                    };
+        if ((buttons[1].textContent !== "") && (buttons[1].textContent === 
+                buttons[4].textContent) && buttons[4].textContent === 
+                    buttons[7].textContent) {
+                        gotWinner(buttons[1]);
+                    };
+        if ((buttons[2].textContent !== "") && (buttons[2].textContent === 
+                buttons[5].textContent) && buttons[5].textContent === 
+                    buttons[8].textContent) {
+                        gotWinner(buttons[2]);
+                    };
+        if ((buttons[0].textContent !== "") && (buttons[0].textContent === 
+                buttons[4].textContent) && buttons[4].textContent === 
+                    buttons[8].textContent) {
+                        gotWinner(buttons[0]);
+                    };
+        if ((buttons[2].textContent !== "") && (buttons[2].textContent === 
+                buttons[4].textContent) && buttons[4].textContent === 
+                    buttons[6].textContent) {
+                        gotWinner(buttons[2]);
+                    };
     }
     displayController();
 })();
